83a0d109c505116f31ed0a16b1279547601331e6
LucasMaack <s204439@student.dtu.dk>
2021-01-04 17:24:12 +0100

    Test

--- src/main.java
+++ src/main.java
@@ -1,5 +1,37 @@
-public class main {
-	public main() {
-		System.out.println("test");
+import javafx.application.Application;
+import javafx.event.*;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import javafx.scene.input.*;
+
+public class main extends Application {
+
+	public static void main(String[] args) {
+		launch(args);
 	}
+
+	private int counter = 0;
+	private Button button = new Button();
+	Stage window;
+
+	@Override
+	public void start(Stage primaryStage) {
+		window = primaryStage;
+		primaryStage.setTitle("Close?");
+		this.button.setText("Close!");
+		this.button.setOnAction(this::handleClick);
+		StackPane root = new StackPane();
+		root.getChildren().add(this.button);
+		Scene scene = new Scene(root, 500, 350);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+		
+	}
+
+	private void handleClick(ActionEvent event) {
+		window.close();
+	}
+	
 }
