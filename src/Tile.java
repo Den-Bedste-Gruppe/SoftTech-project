cce4cb523cd9de3ac70d37717af121e3132e590b
Philip Hviid <philiphviid@gmail.com>
2021-01-08 17:43:08 +0100

    added tile class

---
+++ src/Tile.java
@@ -0,0 +1,20 @@
+
+public class Tile {
+	private int marker;
+	private boolean shown;
+	
+	
+	public boolean isShown() {
+		return shown;
+	}
+	
+	public int getMarker() {
+		return marker;
+	}
+	
+	private void incMarker() {
+		marker++;
+	}
+	
+	
+}
