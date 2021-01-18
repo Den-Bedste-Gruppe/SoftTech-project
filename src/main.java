d6506820c19de27eb7ecc5a9184dfe508dfc6282
Emil Krarup <emil.krarup@live.dk>
2021-01-18 12:01:27 +0100

    game size

--- src/main.java
+++ src/main.java
@@ -24,7 +24,7 @@ public class main extends Application {
 	@Override
 	public void start(Stage primaryStage) throws IOException {
 	
-		MineSweeperController.setGame(new MineSweeperGame(4,4, 17));
+		MineSweeperController.setGame(new MineSweeperGame(4,4, 10));
 		primaryStage = FXMLLoader.load(main.class.getResource("grid2.fxml"));
 		
 
