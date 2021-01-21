a551253dabcfb6c00b8eddf8df4d1b949513e15a
LucasMaack <s204439@student.dtu.dk>
2021-01-21 11:58:29 +0100

    Themes now also affect buttons

--- src/controller/MineSweeperController.java
+++ src/controller/MineSweeperController.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import java.util.Stack;
-
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -14,7 +13,6 @@ import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
-
 import javafx.stage.Stage;
 import main.Main;
 import model.MineSweeperGame;
